b9b4f26be6a08fcf332931fb49b15a27d5979d9a
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-08-15 18:28:54 +0300

    Initial Commit

---
+++ AppleIDExemple/AppleIDExempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleIDExempleApp.swift
+//  AppleIDExemple
+//
+//  Created by Vlad on 15/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleIDExempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
