f910e488b4accfddd00d8a6db0bc52f03bab6bc4
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-08-15 21:43:52 +0300

    worspaces

--- AppleIDExemple/ContentView.swift
+++ AppleIDExemple/ContentView.swift
@@ -6,16 +6,97 @@
 //
 
 import SwiftUI
+import AuthenticationServices
+import LocalAuthentication // for biometric login
 
 struct ContentView: View {
+    
+    @State private var isAuthenticated: Bool = false
+    @State private var showError: Bool = false
+    @State private var errorMessage: String?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            if isAuthenticated {
+                Text("Welcome Back")
+                    .font(.largeTitle)
+                // logic for taking the user to the rest of the app would be here
+                Button("Sign Out") {
+                    isAuthenticated = false
+                }
+                .tint(.red)
+            } else {
+                SignInWithAppleButton(.signIn) {
+                    request in
+                    request.requestedScopes = [.fullName, .email]
+                } onCompletion: { result in
+                    // handle request of the apple sign in process
+                    handleAppleSignIn(result)
+                }
+                .signInWithAppleButtonStyle(.black)
+                .frame(height: 50)
+                .padding()
+                
+                Button {
+                    authenticateWithBioMetrics()
+                } label: {
+                    Text("Login with touch / fase id")
+                        .bold()
+                        .frame(width: 370, height: 50)
+                        .foregroundStyle(.white)
+                        .background(Color.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 6))
+                        .padding(.horizontal)
+                }
+            }
         }
-        .padding()
+        .alert(isPresented: $showError) {
+            // show an alert if authentication is fails
+            Alert(title: Text("Authentication Error"), message: Text(errorMessage ?? "unknown error"), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func handleAppleSignIn(_ result: Result<ASAuthorization, Error>) {
+        switch result {
+        case .success(let authorization):
+            if let _ = authorization.credential as? ASAuthorizationAppleIDCredential {
+                // mark the user as authenticated if the apple id sign in is valid
+                isAuthenticated = true
+            }
+        case .failure(let error):
+            // capture and display any errors from the apple id sign in
+            errorMessage = error.localizedDescription
+            showError = true
+            print("Apple id error \(error.localizedDescription)")
+        }
+    }
+    
+    func authenticateWithBioMetrics() {
+        let context = LAContext()
+        var error: NSError?
+        
+        // check if biometric authentication is avaliable
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Authenticae with Face ID or Touch ID"
+            // attempt to authenticate w/ biometrics
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
+                DispatchQueue.main.async {
+                    if success {
+                        // if successful mark as authentacted
+                        isAuthenticated = true
+                    } else {
+                        // show an error if not authenticated
+                        errorMessage = error?.localizedDescription ?? "Biometric failure"
+                        showError = true
+                    }
+                }
+            }
+        } else {
+            // show an error if biometric is not avaliable
+            errorMessage = error?.localizedDescription ?? "Biometric failure"
+            showError = true
+        }
     }
 }
 
